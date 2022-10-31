6c9afd63d96cafd392c12826a19f005aec50a80d
Sidratul Ahmed <ahmed.bonyeamin@gmail.com>
2022-10-31 21:17:25 +1100

    updated config with my site url

--- astro.config.ts
+++ astro.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
 			wrap: true,
 		},
 	},
-	site: "https://www.astro-theme-cactus.netlify.app",
+	site: "https://sidratul-ahmed.netlify.app",
 	integrations: [
 		mdx({}),
 		tailwind({
